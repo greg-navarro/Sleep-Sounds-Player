e37c7b65015f34307d334a190d8c7e8ab2d888c3
greg-navarro <gregorylitmannavarro@gmail.com>
2021-12-30 16:46:49 -0800

    Complete code for plain old javascript version of the project

--- script.js
+++ script.js
@@ -9,30 +9,43 @@ const soundData = [
 
 // Important values
 const maxVolumeLevel = 100;
-// identify master volume & it's current value ()
-const masterVolume = document.getElementById("master-volume");
+let AUDIORANGES = [];
+
+// identify master volume & it's current value
+const masterVolume = document.querySelector("#master-volume");
 let currentMasterVolume = masterVolume.value;
 
-
-
-// get parent container
+// get parent container for individual volume ranges
 const outerSoundsContainer = document.querySelector("#sounds-container");
 
-// populate outer container with sound objects
+// populate outer container with audio elements/inputs
 initializeSounds(soundData, outerSoundsContainer, masterVolume);
 
+// collect all audio elements, attach event handler to master volume range
+if (masterVolume.addEventListener) {
+  masterVolume.addEventListener("change", function () {
+    currentMasterVolume = masterVolume.value;
+    // go through each audio reference and adjust it's audio
+    for (let [currentRange, currentAudio] of AUDIORANGES) {
+      currentAudio.volume = (currentRange.value / maxVolumeLevel) * (masterVolume.value / maxVolumeLevel);
+    }
+  })
+}
 
-
-
-// functions
-// helper function to set multiple html attributes at once
+/* Functions */
+// Set multiple html attributes at once w/ object literal syntax.
+// element is the HTML whose attributes we are setting,
+// attributes is an object storing mappings of attributes & values. 
 function setAttributes(element, attributes) {
   for (let [attribute, value] of Object.entries(attributes)) {
     element.setAttributes(attribute, value);
   }
 }
 
-// Creates an individual sound/range slider element.
+// Creates an individual sound/range slider element from a given Sound object.
+// Registers an event handlers for individual and master volume controls 
+// (that are Input objects of the range type). Therefore a reference to that 'master volume'
+// Input object is required.
 function createSoundsElement(soundObject, masterVolumeControl) {
   // constants
   const MIN = 0;
@@ -43,7 +56,7 @@ function createSoundsElement(soundObject, masterVolumeControl) {
   let audioElement = document.createElement("audio");
   let audioSourceElement = document.createElement("source");
   setAttributes(audioSourceElement, {
-    src: "https://www.soundjay.com/nature/sounds/ocean-wave-1.mp3", // TODO get audio source from the soundObject
+    src: soundObject.soundSrc, // "https://www.soundjay.com/nature/sounds/ocean-wave-1.mp3", 
     type: "audio/mpeg"
   });
   audioElement.loop = true; // play audio file on repeat
@@ -53,8 +66,8 @@ function createSoundsElement(soundObject, masterVolumeControl) {
   let soundContainer = document.createElement("div"); // TODO .classList.add("sounds-container")
   let soundImage = document.createElement("img");
   setAttributes(soundImage, {
-    src: "",
-    alt: "TEST STRING"  // TODO set src & alt properties of the image
+    src: soundObject.imgSrc,
+    alt: soundObject.name
   });
   let rangeInput = document.createElement("input");
   setAttributes(rangeInput, {
@@ -64,18 +77,24 @@ function createSoundsElement(soundObject, masterVolumeControl) {
     max: MAX.toString(),
     step: STEP.toString()
   });
-  // add event handler to the range
+  // add event handler to individual range input for volume control
   let currentMasterVolumeLevel = masterVolumeControl.value;
   rangeInput.addEventListener("change", function () {
-    const newVolumeLevel = (this.value / MAX) * (currentMasterVolumeLevel / MAX); // get the new volume, between 0 and 1.0 inclusive
-    audioElement.volume = newVolumeLevel; // set the value of the current audio source volume to the new value
+    // get the new volume, between 0 and 1.0 inclusive
+    const newVolumeLevel = (this.value / MAX) * (currentMasterVolumeLevel / MAX);
+    // set the value of the current audio source volume to the new value
+    audioElement.volume = newVolumeLevel;
   }, false);
   // assemble sound slider html
   soundContainer.append(audioElement, soundImage, rangeInput);
+  // add to global array with mappings of audio nodes and their respective ranges
+  let thisMapping = [rangeInput, audioElement];
+  AUDIORANGES.push(thisMapping);
   return soundContainer;
 }
 
-// Given an array of Sound objects, create a list of HTML elements and insert them into the provided node.
+// Given an array of Sound objects, create a list of HTML elements and 
+// insert them into the provided node.
 function initializeSounds(soundsObjects, outerSoundsContainer, masterVolumeControl) {
   // create sound html elements and add them to the document
   for (const soundObject of soundsObjects) {
