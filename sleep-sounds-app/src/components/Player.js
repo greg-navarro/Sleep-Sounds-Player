45a980394f45ac613e96a293a4f5a0bb9e203352
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-07 09:50:39 -0800

    accept and pass data to sound children

--- sleep-sounds-app/src/components/Player.js
+++ sleep-sounds-app/src/components/Player.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import SoundSourceList from './SoundSourceList';
 
-export default function Player() {
+export default function Player(data = []) {
   return (
     <>
       <h1>Sleep Sounds Player</h1>
@@ -11,7 +12,7 @@ export default function Player() {
         </button>
       </div>
       {/* sound ranges */}
-      <div id="sounds-container"></div>
+      <SoundSourceList soundList={data} />
       {/* master volume */}
       <div id="master-volume-container">
         <label for="master-volume">Master volume</label>
