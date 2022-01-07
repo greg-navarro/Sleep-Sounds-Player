92013cdf63758efae3b2f51075c9582773e4b226
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-07 09:51:03 -0800

    import soundData from json and pass it to the Player component

--- sleep-sounds-app/src/App.js
+++ sleep-sounds-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Player from './components/Player';
+import soundData from './data/sounds-data.json';
 
 function App() {
   return (
     <div className="App">
-      <Player />
+      <Player data={soundData} />
     </div>
   );
 }
