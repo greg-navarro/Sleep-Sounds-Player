a2c1ddeb5e6e8d77df1ba3aace92912db947528d
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-07 09:34:38 -0800

    Moves the html into React app

--- sleep-sounds-app/src/components/Player.js
+++ sleep-sounds-app/src/components/Player.js
@@ -4,7 +4,20 @@ export default function Player() {
   return (
     <>
       <h1>Sleep Sounds Player</h1>
-
+      {/* play button */}
+      <div id="play-container">
+        <button name="play-pause" type="button" id="play-pause">
+          PLAY/PAUSE
+        </button>
+      </div>
+      {/* sound ranges */}
+      <div id="sounds-container"></div>
+      {/* master volume */}
+      <div id="master-volume-container">
+        <label for="master-volume">Master volume</label>
+        <input type="range" name="master-volume" id="master-volume" min="0" max="100" step="2">
+        </input>
+      </div>
     </>
   );
 }
