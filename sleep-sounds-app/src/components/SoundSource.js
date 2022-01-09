3cf55118bbe46d70c2d85d4959e30ef647ccecca
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-09 14:17:40 -0800

    add soundsource, and a list of soundsources

---
+++ sleep-sounds-app/src/components/SoundSource.js
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+
+export default function SoundSource(name = "", src = "", img = "") {
+  // constants
+  const MIN = 0;
+  const MAX = 100;
+  const STEP = 2;
+
+  // current volme level
+  let volumeLevel = MAX / 2;
+
+  // declare refs
+  const audioSrc = useRef();
+  const volumeControl = useRef();
+
+  // event handler for volume changes
+  const adjustVolume = () => {
+    // get new volume level
+    // set audio to new level  
+  };
+
+  return (
+    <div>
+      <audio ref={audioSrc} src={src} />
+      <img src={img} alt={name}></img>
+      <input ref={volumeControl} type="range" name="soundSourceVolume" id="" min={MIN.toString()} max={MAX.toString()} step={STEP.toString()}></input>
+    </div>
+  );
+}
--- sleep-sounds-app/src/components/SoundSourceList.js
+++ sleep-sounds-app/src/components/SoundSourceList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SoundSource from "./SoundSource";
 
 export default function SoundSourceList(soundList = []) {
   return (
     <div id="sounds-container">
-      { }
+      {soundList.map(sound => <SoundSource name={sound.name} src={sound.src} img={sound.imgSrc} />)}
     </div>
   );
 }
