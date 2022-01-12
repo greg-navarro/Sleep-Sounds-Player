a141c817976ed84c3e459e78d647e7f20d968b85
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-12 11:07:27 -0800

    updates master volume correctly

--- sleep-sounds-app/src/components/SoundSource.js
+++ sleep-sounds-app/src/components/SoundSource.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 
-export default function SoundSource({ name = "", src = "", img = "", min = 0, max = 100, step = 2, getMasterVolume = f => f }) {
+export default function SoundSource({ name = "", src = "", img = "", min = 0, max = 100, step = 2, getMasterVolume = f => f, id = "" }) {
   // constants
   const MIN = min;
   const MAX = max;
@@ -10,6 +10,10 @@ export default function SoundSource({ name = "", src = "", img = "", min = 0, ma
   let volumeLevel = MAX / 2;
   let currentMasterVolume = getMasterVolume();
 
+  // set id's for audio element and range
+  const audioID = "audio" + id.toString();
+  const rangeID = "range" + id.toString();
+
   // declare refs
   const audioSrc = useRef();
   const volumeControl = useRef();
@@ -17,8 +21,10 @@ export default function SoundSource({ name = "", src = "", img = "", min = 0, ma
   // event handler for volume changes
   const adjustVolume = (e) => {
     // TODO adjust volume of track based ONLY on the current range value
-    // get new volume level
-    const newVolumeLevel = e.target.value / MAX * currentMasterVolume;
+    // check for changes in master volume level
+    currentMasterVolume = getMasterVolume();
+    console.log(currentMasterVolume);
+    const newVolumeLevel = (e.target.value / MAX) * currentMasterVolume;
     // console.log(e);
     // set audio to new level
     audioSrc.current.volume = newVolumeLevel;
@@ -27,13 +33,13 @@ export default function SoundSource({ name = "", src = "", img = "", min = 0, ma
 
   return (
     <div>
-      <audio ref={audioSrc} src={src} className="audio-element" />
+      <audio ref={audioSrc} src={src} className="audio-element" id={audioID} />
       <img src={img} alt={name}></img>
       <input
         ref={volumeControl}
         type="range"
         name="soundSourceVolume"
-        id=""
+        id={rangeID}
         min={MIN.toString()}
         max={MAX.toString()}
         step={STEP.toString()}
--- sleep-sounds-app/src/components/SoundSourceList.js
+++ sleep-sounds-app/src/components/SoundSourceList.js
@@ -1,12 +1,11 @@
 import React from "react";
 import SoundSource from "./SoundSource";
 
-export default function SoundSourceList({ soundList = [] }) {
-
+export default function SoundSourceList({ soundList = [], getMasterVolume = f => f }) {
   return (
     <div id="sounds-container">
       {soundList.map(sound => (
-        <SoundSource key={sound.name} name={sound.name} src={sound.src} img={sound.imgSrc} />
+        <SoundSource key={sound.name} name={sound.name} src={sound.src} img={sound.imgSrc} id={sound.id} getMasterVolume={getMasterVolume} />
       ))}
     </div>
   );
