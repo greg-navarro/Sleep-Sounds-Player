21e06e2a1e426d69bc298a0aa5d5f66725bdbf66
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-13 14:49:48 -0800

    gets the search bar on the Home page to render

--- sleep-sounds-app/src/App.js
+++ sleep-sounds-app/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 // import './App.css';
 import { Navbar } from './components/Navbar';
+import Home from './components/Home';
 import Player from './components/Player';
 import soundData from './data/sounds-data.json';
 
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Player data={[...soundData]} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/oldeplayer" element={<Player data={[...soundData]} />} />
       </Routes>
 
---
+++ sleep-sounds-app/src/components/Home.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { v4 } from "uuid";
+
+export default function Home({ presents = [] }) {
+  // FIXME add test data (remove once data passed from parent)
+  presents.push({
+    id: v4(),
+    name: "groovy beach sounds"
+  });
+
+  // add search query to state (controlled component)
+  const [query, setQuery] = useState("");
+  // add the presents currently showing to the state
+  const [displayedPresets, setDisplayedPresents] = useState(presents);
+
+  // filter list of all presents for those that resemble the search pattern
+  const filterResults = () => {
+    const queryResults = presents.map((present) => present.name.includes(query));
+    setDisplayedPresents(queryResults);
+  };
+
+  // // perform initial filtering TODO only if the search parameter is included
+  // filterResults();
+
+  return (
+    <div className="presents">
+      {/* TODO implement search bar */}
+      <div>
+        <input value={query} type="text" name="present-query" placeholder="Search presents" onChange={event => setQuery(event.target.value)} />
+        <button onClick={filterResults}>Search</button>
+      </div>
+      {/* TODO make each button a link that takes use to a detail page w/ id */}
+      {displayedPresets.map((present, i) => (
+        <div key={i} >
+          <p>{present.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
