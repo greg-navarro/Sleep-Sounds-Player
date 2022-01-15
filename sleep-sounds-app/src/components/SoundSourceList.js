3bb3ca0e7bdf243f8058b412a0851ef96d4caf39
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-14 16:35:58 -0800

    Move the 'add sound' elements into the SoundSourceList comp

--- sleep-sounds-app/src/components/SoundSourceList.js
+++ sleep-sounds-app/src/components/SoundSourceList.js
@@ -1,21 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import SoundSource from "./SoundSource";
 
 export default function SoundSourceList({
-  soundList = [],
+  presents = [],
+  otherSounds = [],
   getMasterVolume = f => f }) {
+  const [soundSourceElements, addSoundSourceElement] = useState([]);
+  // function to create Sound Source elements
+  const initializeSoundSource = (sound) => {
+    return (
+      <SoundSource
+        key={sound.name}
+        name={sound.name}
+        src={sound.src}
+        img={sound.imgSrc}
+        id={sound.id}
+        getMasterVolume={getMasterVolume}
+      />);
+  };
+
+  // const [selectedOption, setSelectedOption] = useState(options[0].value);
+  const addSoundSource = (sourceIndex) => {
+    const sourceToAdd = otherSounds[sourceIndex];
+    const newSource = initializeSoundSource(sourceToAdd);
+    soundSourceElements.push(newSource);
+    addSoundSourceElement(soundSourceElements);
+  };
+
+
+
   return (
-    <div id="sounds-container">
-      {soundList.map(sound => (
-        <SoundSource
-          key={sound.name}
-          name={sound.name}
-          src={sound.src}
-          img={sound.imgSrc}
-          id={sound.id}
-          getMasterVolume={getMasterVolume}
-        />
-      ))}
-    </div>
+    <>
+      <div id="sounds-container">
+        {presents.map(sound => (
+          <SoundSource
+            key={sound.name}
+            name={sound.name}
+            src={sound.src}
+            img={sound.imgSrc}
+            id={sound.id}
+            getMasterVolume={getMasterVolume}
+          />
+        ))}
+      </div>
+      {/* Add additional sounds */}
+      <div>
+        <label htmlFor="otherSounds">Add sound</label>
+        <select
+          name="otherSounds"
+          value="Sounds"
+          onChange={(e) => {
+            e.preventDefault();
+            addSoundSource(e.target.value);
+          }}
+        >
+          <option value="Select a sound">Select a sound</option>
+          {otherSounds.map((sound, i) =>
+          (
+            <option
+              key={i}
+              value={i}
+            >
+              {sound.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 }
