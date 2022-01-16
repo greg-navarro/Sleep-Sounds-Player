4602064bdfe4a435e8bde8af9ae7e21a883b1d1b
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-16 10:07:25 -0800

    Do several silly things to process the data. This will be refactored OUT down the line. Very soon.

--- sleep-sounds-app/src/components/Player.js
+++ sleep-sounds-app/src/components/Player.js
@@ -1,13 +1,16 @@
 import React, { useState, useRef, createContext } from 'react';
+import { useParams } from 'react-router-dom';
 import SoundSourceList from './SoundSourceList';
 import { v4 } from "uuid";
 
 export const PlayerContext = createContext(); // FIXME adds context and exports
 
-export default function Player({ present = [], otherSounds = [] }) {
+export default function Player({ present = null, otherSounds = [] }) {
   // FIXME test othersounds and presents
-  otherSounds = otherSounds.length === 0 ? present : otherSounds;
+  // otherSounds = otherSounds.length === 0 ? present : otherSounds;
   // console.group(present);
+
+
 
   const masterVolumeRef = useRef();
   const maxVolumeLevel = 100;
@@ -18,6 +21,31 @@ export default function Player({ present = [], otherSounds = [] }) {
   // we need to associate a unique id with each sound source.
   const initialSoundSources = present.map(sound => ({ ...sound, id: v4() }));
   const [soundSources, setSoundSources] = useState(initialSoundSources);
+
+  // Step 1: is the present present? (if not, player is custom)
+  const { id } = useParams();
+  const presentFromParam = otherSounds.find(sound => sound.id === id);
+  // Step 2 (w/ present): filter otherSounds for the sounds in present, then remove these from otherSounds
+  let unusedSounds = otherSounds;
+  if (present) {
+    const soundSourcesFromPresent = [];
+    for (const soundID of present.sounds) {
+      const soundObject = otherSounds.find(obj => obj.id === soundID);
+      soundSourcesFromPresent.push(soundObject);
+      unusedSounds = unusedSounds.filter(sound => sound.id !== soundID);
+    }
+    setSoundSources([...soundSources, ...soundSourcesFromPresent]);
+  } else if (presentFromParam) {
+    const soundSourcesFromPresent = [];
+    for (const soundID of presentFromParam.sounds) {
+      const soundObject = otherSounds.find(obj => obj.id === soundID);
+      soundSourcesFromPresent.push(soundObject);
+      unusedSounds = unusedSounds.filter(sound => sound.id !== soundID);
+    }
+    setSoundSources([...soundSources, ...soundSourcesFromPresent]);
+  }
+
+
 
   let justLoaded = true;
   const play = () => {
@@ -79,7 +107,7 @@ export default function Player({ present = [], otherSounds = [] }) {
         </button>
       </div>
       {/* sound ranges */}
-      <SoundSourceList present={present} otherSounds={otherSounds} getMasterVolume={getMasterVolume} registerNewSound={registerNewSound} masterVolumeRef={masterVolumeRef} />
+      <SoundSourceList present={present} otherSounds={unusedSounds} getMasterVolume={getMasterVolume} registerNewSound={registerNewSound} masterVolumeRef={masterVolumeRef} />
       {/* master volume */}
       <div id="master-volume-container">
         <label htmlFor="master-volume">Master volume</label>
